37447e68d353bc95153cad848b903f7d619585ad
alex-alk <alex.leo06@yahoo.ro>
2025-07-22 16:16:54 +0300

    update

--- main.js
+++ main.js
@@ -2,5 +2,5 @@ export function createComponent(html) {
     const template = document.createElement('template')
     template.innerHTML = html
     const component = template.content
-    return component
+    return component.firstElementChild
 }
